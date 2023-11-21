e7406267b8304bf3876a0901eae961b4b49cdb91
SeRDic0705 <serdic0705@gmail.com>
2023-11-21 14:37:56 +0900

    refactor: 버전충돌로 kotlin에서 java로 프로젝트 수정

--- salida/build.gradle.kts
+++ salida/build.gradle.kts
@@ -1,18 +1,14 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-	war
+	java
 	id("org.springframework.boot") version "3.1.5"
 	id("io.spring.dependency-management") version "1.1.3"
-	kotlin("jvm") version "1.8.22"
-	kotlin("plugin.spring") version "1.8.22"
 }
 
 group = "com.taba4"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_17
+	sourceCompatibility = JavaVersion.VERSION_21
 }
 
 configurations {
@@ -27,20 +23,10 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	annotationProcessor("org.projectlombok:lombok")
-	providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-}
-
-tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs += "-Xjsr305=strict"
-		jvmTarget = "17"
-	}
 }
 
 tasks.withType<Test> {
---
+++ salida/src/main/java/com/taba4/salida/SalidaApplication.java
@@ -0,0 +1,13 @@
+package com.taba4.salida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalidaApplication.class, args);
+	}
+
+}
