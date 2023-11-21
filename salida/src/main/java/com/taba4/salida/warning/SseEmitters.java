4733475c69077cf3091a581bafa60e0d7473b239
SeRDic0705 <serdic0705@gmail.com>
2023-11-21 15:37:18 +0900

    feat: 재난알림 기능 추가
    SseEmitter를 이용한 재난 정보 알림 기능

---
+++ salida/src/main/java/com/taba4/salida/warning/SseEmitters.java
@@ -0,0 +1,77 @@
+package com.taba4.salida.warning;
+
+import com.taba4.salida.warning.dto.EqkDto;
+import com.taba4.salida.warning.dto.WarDto;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@Component
+@Slf4j
+public class SseEmitters {
+    private final List<SseEmitter> emitters = new CopyOnWriteArrayList<>();
+
+    public SseEmitter connect(SseEmitter emitter) {
+        this.emitters.add(emitter);
+        log.info("new emitter connected: {}", emitter);
+        log.info("emitters size: {}", emitters.size());
+        log.info("emitter list: {}", emitters);
+
+        emitter.onCompletion(() -> {
+            log.info("onCompletion callback");
+            this.emitters.remove(emitter);
+        });
+        emitter.onTimeout(() -> {
+            log.info("onTimeout callback");
+            this.emitters.remove(emitter);
+        });
+        return emitter;
+    }
+
+    public void warnEqk(EqkDto eqkData) {     //위도 경도를 받아 emitters에 뿌리기
+        emitters.forEach(emitter -> {
+            try {
+                emitter.send(SseEmitter.event()
+                        .name("earthquake")
+                        .data(eqkData, MediaType.APPLICATION_JSON));
+            } catch (IOException e) {
+                throw new RuntimeException();
+            }
+        });
+    }
+
+    public void warnWar(WarDto warData) {
+        emitters.forEach(emitter -> {
+            try {
+                emitter.send(SseEmitter.event()
+                        .name("war")
+                        .data(warData, MediaType.APPLICATION_JSON));
+            } catch (IOException e) {
+                throw new RuntimeException();
+            }
+        });
+    }
+
+    /*
+    public long test() {
+        long dummydata = 3;
+        emitters.forEach(emitter -> {
+            try {
+                emitter.send(SseEmitter.event()
+                        .name("test")
+                        .data(dummydata));
+            } catch (IOException e) {
+                throw new RuntimeException();
+            }
+        });
+        return dummydata;
+    }
+     */
+
+}
---
+++ salida/src/main/java/com/taba4/salida/warning/dto/WarDto.java
@@ -0,0 +1,8 @@
+package com.taba4.salida.warning.dto;
+
+import lombok.Getter;
+
+@Getter
+public class WarDto {
+    private String warInfo;
+}
