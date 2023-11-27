5c8a4da7dcc86f847791744719eb2a65a84b0efb
kek <alexeun@naver.com>
2023-11-27 13:32:04 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask
+import pyodbc
+
+connection_string = 'DSN=tibero-test;UID=sys;PWD=tibero'
+connection = pyodbc.connect(connection_string)
+cursor = connection.cursor()
+
+app = Flask(__name__)
+
+select_query = '''
+select * from emergency_news;
+'''
+
+cursor.execute(select_query)
+
+@app.route('/')
+def hello_world():
+
+    while True:
+        row = cursor.fetchone()
+        if not row:
+            break
+        print(row)
+
+    connection.close()
+
+    return 'Hello World!'
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    app.run()
+
