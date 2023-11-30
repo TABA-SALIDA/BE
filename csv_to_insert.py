a0c79d9c2f552ae5cf5c65e36acc2c419cd20f67
EC2 Default User <ec2-user@ip-172-31-61-33.ap-northeast-2.compute.internal>
2023-11-30 07:44:29 +0000

    예외 처리

--- csv_to_insert.py
+++ csv_to_insert.py
@@ -11,49 +11,63 @@ DSN = 'tibero6'
 username = 'sys'
 password = 'tibero'
 
+try:
+    conn = pyodbc.connect(f'DSN={DSN};UID={username};PWD={password}')
+    conn.setdecoding(pyodbc.SQL_CHAR, encoding='utf-16')
+    conn.setdecoding(pyodbc.SQL_WCHAR, encoding='utf-16')
+    conn.setencoding(encoding='utf-8')
 
-conn = pyodbc.connect(f'DSN={DSN};UID={username};PWD={password}')
-conn.setdecoding(pyodbc.SQL_CHAR, encoding='utf-16')
-conn.setdecoding(pyodbc.SQL_WCHAR, encoding='utf-16')
-conn.setencoding(encoding='utf-8')
-
-cursor = conn.cursor()
-
+    cursor = conn.cursor()
+except Exception as e:
+    print(f"Database connection failed: {e}")
+    exit(1)
 
 # Function to insert data from CSV to database
 def insert_data_from_csv(file_path):
-    # Read CSV file
-    df = pd.read_csv(file_path)
-    # Assuming table and columns already exist and match CSV format
-    # Replace 'YourTableName' with your actual table name
-
+    try:
+        df = pd.read_csv(file_path)
+    except Exception as e:
+        print(f"Error reading file {file_path}: {e}")
+        return
 
     for index, row in df.iterrows():
-        insert_query = f'''
-    INSERT INTO emergency_news
-    VALUES (s_news_id.NEXTVAL, 
-            ?, 
-            ?,
-            ?, 
-            ?, 
-            'earthquack')
-    '''
-        values = [row['title'], row['content'], row['summary'], row['link']]
-        cursor.execute(insert_query, values)
+        try:
+            insert_query = f'''
+                            INSERT INTO emergency_news
+                            VALUES (s_news_id.NEXTVAL, 
+                            ?, 
+                            ?,
+                            ?, 
+                            ?, 
+                            'earthquack')
+                            '''
+            values = [row['title'], row['content'], row['summary'], row['link']]
+            cursor.execute(insert_query, values)
+        except Exception as e:
+            print(f"Failed to insert data: {e}")
 
-    conn.commit()
+    try:
+        conn.commit()
+    except Exception as e:
+        print(f"Failed to commit transaction: {e}")
 
 # Monitoring the directory
-process = subprocess.Popen(['inotifywait', '-m', '-e', 'create', '-e', 'modify','-e', 'close_write', '--format', '%w%f', directory_to_watch], stdout=subprocess.PIPE)
+try:
+    process = subprocess.Popen(['inotifywait', '-m', '-e', 'create', '-e', 'modify','-e', 'close_write', '--format', '%w%f', directory_to_watch], stdout=subprocess.PIPE)
+except Exception as e:
+    print(f"Failed to start directory monitoring: {e}")
+    exit(1)
 
 while True:
-    line = process.stdout.readline()
-    if not line:
-        break
-    filename = line.strip().decode()
+    try:
+        line = process.stdout.readline()
+        if not line:
+            break
+        filename = line.strip().decode()
 
     # Check if the file is a CSV
-    if filename.endswith('.csv'):
-        print(filename)
-        insert_data_from_csv(filename)
+        if filename.endswith('.csv'):
+            insert_data_from_csv(filename)
+    except Exception as e:
+        print(f"Error during file processing: {e}")
 
--- readtest.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('/home/ec2-user/data/NaverNews_지진.csv')
-print(df)
